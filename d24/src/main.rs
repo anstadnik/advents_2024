cdf21989a8b0d98f784dc5214ada71d52cf39f1e
Stadnik Andrii <stadnikandriy1@gmail.com>
2025-01-15 22:17:35 +0200

    --wip--

--- d24/src/main.rs
+++ d24/src/main.rs
@@ -1,8 +1,10 @@
 mod parse;
+mod tests;
 
-use anyhow::{anyhow, Result};
+use anyhow::{bail, Result};
+use indicatif::ProgressIterator;
 use parse::{parse_intput, Ops, State};
-use std::fs::read_to_string;
+use std::{collections::HashMap, fs::read_to_string, mem::swap};
 
 fn execute<'a>(mut wires: State<'a>, ops: &'a Ops<'a>) -> Result<State<'a>> {
     let mut ops = ops.clone();
@@ -17,7 +19,9 @@ fn execute<'a>(mut wires: State<'a>, ops: &'a Ops<'a>) -> Result<State<'a>> {
                 true
             }
         });
-        assert!(ops.len() < n, "No progress");
+        if ops.len() == n {
+            bail!("No progress");
+        }
     }
 
     Ok(wires)
@@ -44,24 +48,108 @@ fn test_task_2_(mut init: State, x: N, y: N, ops: &Ops) -> Result<bool> {
         *init.get_mut(format!("y{i:02}").as_str()).unwrap() = (y >> i) & 1 == 1
     }
     Ok(task1(init, ops)? == x + y)
+    //Ok(task1(init, ops)? == x & y)
 }
 
-fn test_task_2(init: State, ops: &Ops) -> Result<N> {
-    let n_bits = init.len() / 2;
-    let x = 0..1 << n_bits;
-    let y = 0..1 << n_bits;
-    x.zip(y)
-        .find_map(|(x, y)| {
-            test_task_2_(init.clone(), x, y, ops)
-                .ok()
-                .filter(|&b| b)
-                .map(|_| x + y)
+fn find_fault_bit_task_2(init: State, ops: &Ops) -> Option<usize> {
+    (0..init.len() / 2).find(|&i| {
+        let x = (1 << i) - 1;
+        let y = 1;
+        //let x = 1 << i;
+        //let y = 1 << i;
+        !test_task_2_(init.clone(), x, y, ops).unwrap_or(false)
+    })
+}
+
+fn gen_depsendencies_for<'a>(ops: &Ops<'a>, v: &'a str) -> Vec<&'a str> {
+    if let Some(&(a, _, b, _)) = ops.iter().find(|a| a.3 == v) {
+        [v].into_iter()
+            .chain(gen_depsendencies_for(ops, a))
+            .chain(gen_depsendencies_for(ops, b))
+            .filter(|v| !v.starts_with('x') && !v.starts_with('y'))
+            .collect()
+    } else {
+        vec![v]
+    }
+}
+
+fn gen_dependencies<'a>(ops: &Ops<'a>) -> HashMap<usize, Vec<&'a str>> {
+    ops.iter()
+        .filter(|a| a.3.starts_with('z'))
+        .fold(HashMap::new(), |mut acc, a| {
+            acc.insert(
+                a.3.strip_prefix('z').unwrap().parse().unwrap(),
+                gen_depsendencies_for(ops, a.3),
+            );
+            acc
         })
-        .ok_or_else(|| anyhow!("No solution"))
+}
+
+fn task2_<'a>(
+    init: State<'a>,
+    ops: &Ops<'a>,
+    prev_faulty_bit: Option<usize>,
+    deps: &HashMap<usize, Vec<&'a str>>,
+) -> Result<Option<Vec<&'a str>>> {
+    let mut ops = ops.clone();
+    if let Some(faulty_bit) = find_fault_bit_task_2(init.clone(), &ops) {
+        if prev_faulty_bit.is_some_and(|prev_| faulty_bit <= prev_) {
+            return Ok(None);
+        }
+
+        println!(
+            "Faulty bit: {}, prev_faulty_bit: {:?}",
+            faulty_bit, prev_faulty_bit
+        );
+        let deps_ = deps.get(&faulty_bit).unwrap();
+        // TODO: Do I need ..= ?
+        let deps_to_ignore: Vec<_> = (0..=faulty_bit)
+            .flat_map(|i| deps.get(&i).unwrap())
+            .copied()
+            .collect();
+        println!("Dependencies: {:?}", deps_);
+        let deps_is: Vec<_> = deps_
+            .iter()
+            .map(|v| ops.iter().position(|a| &a.3 == v).unwrap())
+            .collect();
+
+        for mut i in deps_is.into_iter().progress() {
+            for mut j in 0..ops.len() {
+                if j == i || deps_to_ignore.contains(&ops[j].3) {
+                    //if j == i {
+                    continue;
+                }
+
+                if j < i {
+                    swap(&mut j, &mut i);
+                }
+                //println!("Swapping {} and {}", i, j);
+
+                let (a, b) = ops.split_at_mut(j);
+                swap(&mut a[i].3, &mut b[0].3);
+
+                if let Ok(Some(mut v)) = task2_(init.clone(), &ops, Some(faulty_bit), deps) {
+                    v.extend([ops[i].3, ops[j].3]);
+                    return Ok(Some(v));
+                }
+
+                let (a, b) = ops.split_at_mut(j);
+                swap(&mut a[i].3, &mut b[0].3);
+            }
+        }
+        Ok(None)
+    } else {
+        Ok(Some(Vec::new()))
+    }
 }
 
 fn task2(init: State, ops: &Ops) -> Result<String> {
-    let n = ops.len();
+    let deps = gen_dependencies(ops);
+    task2_(init, ops, None, &deps).map(|v| {
+        let mut v = v.unwrap();
+        v.sort_unstable();
+        v.join(",")
+    })
 }
 
 fn main() -> Result<()> {
@@ -72,89 +160,4 @@ fn main() -> Result<()> {
     println!("Answer 2: {}", task2(init, &ops)?);
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use anyhow::Result;
-
-    #[test]
-    fn test_task1() -> Result<()> {
-        let s = r"x00: 1
-x01: 1
-x02: 1
-y00: 0
-y01: 1
-y02: 0
-
-x00 AND y00 -> z00
-x01 XOR y01 -> z01
-x02 OR y02 -> z02";
-        let (init, ops) = parse_intput(s)?;
-
-        assert_eq!(init.len(), 6);
-        assert_eq!(ops.len(), 3);
-
-        assert_eq!(task1(init.clone(), &ops)?, 4);
-        assert_eq!(task2(init, &ops)?, "z00,z01,z02,z05");
-
-        Ok(())
-    }
-    #[test]
-    fn test_task2() -> Result<()> {
-        let s = r"x00: 1
-x01: 0
-x02: 1
-x03: 1
-x04: 0
-y00: 1
-y01: 1
-y02: 1
-y03: 1
-y04: 1
-
-ntg XOR fgs -> mjb
-y02 OR x01 -> tnw
-kwq OR kpj -> z05
-x00 OR x03 -> fst
-tgd XOR rvg -> z01
-vdt OR tnw -> bfw
-bfw AND frj -> z10
-ffh OR nrd -> bqk
-y00 AND y03 -> djm
-y03 OR y00 -> psh
-bqk OR frj -> z08
-tnw OR fst -> frj
-gnj AND tgd -> z11
-bfw XOR mjb -> z00
-x03 OR x00 -> vdt
-gnj AND wpb -> z02
-x04 AND y00 -> kjc
-djm OR pbm -> qhw
-nrd AND vdt -> hwm
-kjc AND fst -> rvg
-y04 OR y02 -> fgs
-y01 AND x02 -> pbm
-ntg OR kjc -> kwq
-psh XOR fgs -> tgd
-qhw XOR tgd -> z09
-pbm OR djm -> kpj
-x03 XOR y03 -> ffh
-x00 XOR y04 -> ntg
-bfw OR bqk -> z06
-nrd XOR fgs -> wpb
-frj XOR qhw -> z04
-bqk OR frj -> z07
-y03 OR x01 -> nrd
-hwm AND bqk -> z03
-tgd XOR rvg -> z12
-tnw OR pbm -> gnj";
-        let (init, ops) = parse_intput(s)?;
-
-        assert_eq!(task1(init.clone(), &ops)?, 2024);
-        assert_eq!(task2(init, &ops)?, "aaa,aoc,bbb,ccc,eee,ooo,z24,z99");
-
-        Ok(())
-    }
 }
